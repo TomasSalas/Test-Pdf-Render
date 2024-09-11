a73bf0810f8b50eef5350cee49d806d1de8489a4
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-09-11 11:18:52 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import '@fontsource-variable/plus-jakarta-sans/wght-italic.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
